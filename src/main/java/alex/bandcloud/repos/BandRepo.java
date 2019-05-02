533e904bfe4fa87308b198040238fc9539297d00
AlexBobryshev <darknessrider1@rambler.ru>
2019-05-02 23:55:55 +0300

    11th commit (reading the list from DB)

--- src/main/java/alex/bandcloud/repos/BandRepo.java
+++ src/main/java/alex/bandcloud/repos/BandRepo.java
@@ -3,6 +3,8 @@ package alex.bandcloud.repos;
 import alex.bandcloud.model.Band;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BandRepo extends CrudRepository<Band, Long> {
+import java.util.List;
 
+public interface BandRepo extends CrudRepository<Band, Long> {
+    List<Band> findDistinctByGenre(Band.Genre genre);
 }
--- src/main/java/alex/bandcloud/web/BandsController.java
+++ src/main/java/alex/bandcloud/web/BandsController.java
@@ -1,17 +1,44 @@
 package alex.bandcloud.web;
 
 import alex.bandcloud.model.Band;
+import alex.bandcloud.repos.BandRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/bands")
 public class BandsController {
+    private BandRepo bandRepo;
+
+    @Autowired
+    public BandsController(BandRepo bandRepo) {
+        this.bandRepo = bandRepo;
+    }
+
+    @ModelAttribute
+    public void addBandsToModel(Model model) {
+        List<Band> list = new ArrayList<>();
+        Band.Genre[] genres = Band.Genre.values();
+
+        for(Band.Genre genre : genres) {
+            for(Band band : bandRepo.findDistinctByGenre(genre)) {
+                list.add(band);
+            }
+        }
+        model.addAttribute("bands", list);
+
+    }
+
     @GetMapping
     public String showCreatedBands() {
-
         return "bands";
     }
 }
